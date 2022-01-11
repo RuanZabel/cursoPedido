c04f44cf6aeba96c78567e1bae90c9ca3b66a9f4
Ruan Carlos Crepes Soares Zabel <ruancrepeszabel@hotmail.com>
2022-01-11 20:48:49 -0300

    criaçao automagica do banco h2

--- src/main/java/com/projetopedido/pedido/PedidoApplication.java
+++ src/main/java/com/projetopedido/pedido/PedidoApplication.java
@@ -1,13 +1,30 @@
 package com.projetopedido.pedido;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PedidoApplication {
+import com.projetopedido.pedido.domain.Categoria;
+import com.projetopedido.pedido.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class PedidoApplication implements CommandLineRunner{ // esse implementes é para iniciar iniciar alguma coisa
+	@Autowired
+	private CategoriaRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(PedidoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1=new Categoria(null, "informática");
+		Categoria cat2=new Categoria(null, "escritorio");
+		repository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
